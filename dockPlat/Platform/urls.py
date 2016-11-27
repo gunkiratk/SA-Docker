6831ff871b50ea9c681583e75fc882970061e2d4
kushagra7589 <kushagra15049@iiitd.ac.in>
2016-11-28 00:56:24 +0530

    urls added

---
+++ dockPlat/Platform/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+
+app_name='Docker'
+
+urlpatterns=[
+	url(r'^',views.login,name='login'),
+	url(r'^register',views.register,name='register'),
+	url(r'^dashboard',views.dashboard,name='register'),
+	url(r'^dashboard/run',views.run_container,name='run'),
+	url(r'^dashboard/view',views.view_containers,name='view'),
+	url(r'^logout',views.logout,name='logout'),
+]
