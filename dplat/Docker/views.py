eddbc0e34df1b9ef69ce8f233c92aeaa6a9b980b
gunkiratk <gunkirat15032@iiitd.ac.in>
2016-11-27 21:56:46 +0530

    view,template

--- dplat/Docker/views.py
+++ dplat/Docker/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from models import Name_pass,Container
-from 
+# from docker import Client
 def home_page(request):
 	return render(request,"register.html",{})
 def register(request):
 	if(request.method=='POST'):
 		name=request.POST['name']
 		password=request.POST['password']
-	return render(request,"utility.html",{'username':name})
+		Username=request.POST['username']
+	return render(request,"utility.html",{'username':Username})
 def run(request):
 	return render(request,"run.html",{})
 def view(request):
