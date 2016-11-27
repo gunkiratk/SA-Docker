c8436bf056aee02d42243db66c73fedc43924183
kushagra7589 <kushagra15049@iiitd.ac.in>
2016-11-27 21:33:35 +0530

    new changes

--- dplat/Docker/views.py
+++ dplat/Docker/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from models import Name_pass,Container
+from 
 def home_page(request):
 	return render(request,"register.html",{})
 def register(request):
-	print "register"
 	if(request.method=='POST'):
 		name=request.POST['name']
 		password=request.POST['password']
-	return render(request,"utility.html",{'username':name})	
+	return render(request,"utility.html",{'username':name})
+def run(request):
+	return render(request,"run.html",{})
+def view(request):
+	return render(request,"view.html",{})
 # Create your views here.
